0b576c738ddc84476e56d6697492f9f0c8631d7f
Jacob Moores <128637985+JMoores13@users.noreply.github.com>
2025-05-23 09:50:15 -0400

    Add files via upload

---
+++ Models/FelmUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SMARTV3.Models
+{
+    public class FelmUpdate
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
