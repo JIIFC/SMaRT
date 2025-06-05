9fca05822bc34a09004b17fae4208d2aedba0d98
Jacob Moores <128637985+JMoores13@users.noreply.github.com>
2025-06-05 11:03:56 -0400

    Update FelmUpdate.cs
    For the frappe version

--- Models/FelmUpdate.cs
+++ Models/FelmUpdate.cs
@@ -5,8 +5,8 @@ namespace SMARTV3.Models
     public class FelmUpdate
     {
         public int Id { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
 
     }
 }
